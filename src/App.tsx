db2fe16f21e4819ddc282db8fc8de11574820f82
Mihai <mihai.muscalu@student.htldornbirn.at>
2024-12-17 10:55:03 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Card from "./assets/Components/Card";
+import "./App.css"
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+    </h1>
+  )
+}
---
+++ src/assets/Components/Card.tsx
@@ -0,0 +1,21 @@
+
+type Props = {}
+
+export default function Card({}: Props) {
+  return (
+    <div>
+        <div><img src="https://picsum.photos/200/300" alt="Woman" /></div>
+        <div>
+            <h1>
+            Susi Quattro
+                <p>
+                Lorem ipsum dolor sit amet consectetur. Quis vitae ut quam urna ut turpis. Nunc imperdiet augue dis suspendisse
+                </p>   
+                
+            </h1>
+            
+        </div>
+        
+    </div>
+  )
+}
